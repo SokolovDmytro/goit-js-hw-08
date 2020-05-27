c3d81b4eb6119fbe220cce7dd22673900da162ab
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-05-27 12:46:32 +0300

    fixed.map

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 "use strict";
 
-import galleryItems from "./gallery-items.js";
+import galleryItems from './gallery-items.js';
 
-const jsGallery = document.querySelector(".js-gallery")
-const lightbox = document.querySelector(".lightbox");
-const lightboxImage = document.querySelector(".lightbox__image");
-const lightboxButton = document.querySelector(".lightbox__button");
+const jsGallery = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+let currentItem = null;
 
-for (let image of galleryItems) jsGallery.insertAdjacentHTML("beforeEnd",
-  `<li>
-    <a
-    class="gallery__link"
-    href="${image.original}"
-    >
-    <img src="${image.preview}" 
-    alt="${image.description}" 
-    data-source="${image.original}"
-    class="gallery__image">
-    </a>
-    </li>`
-);
+function createGallery(items) {
+  return items
+    .map(({
+      preview,
+      original,
+      description
+    }) => {
+      return `
+    <li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
+    })
+    .join('');
+}
+jsGallery.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 
-function openModal(e) {
+jsGallery.addEventListener('click', showModal);
+
+function showModal(e) {
+  if (e.target.nodeName !== 'IMG') return;
+  currentItem = e.target.closest('li');
   e.preventDefault();
-  lightbox.classList.add("is-open");
-  lightboxImage.src = e.target.dataset.source;
+  lightbox.classList.add('is-open');
+  lightboxImage.setAttribute('src', e.target.dataset.source);
+  lightboxImage.setAttribute('alt', e.target.alt);
+  lightbox.addEventListener('click', handelLightboxClick);
+  closeBtn.addEventListener('click', closeModal);
+}
+
+function handelLightboxClick(evn) {
+  if (evn.target.nodeName === 'IMG') return;
+  closeModal();
 }
 
 function closeModal() {
-  lightbox.classList.remove("is-open");
-  lightboxImage.src = '';
+  lightbox.classList.remove('is-open');
+  lightboxImage.setAttribute('src', '');
+  lightboxImage.setAttribute('alt', '');
+  lightbox.removeEventListener('click', handelLightboxClick);
+  closeBtn.removeEventListener('click', closeModal);
 }
-
-jsGallery.addEventListener("click", openModal);
-lightboxButton.addEventListener("click", closeModal);
